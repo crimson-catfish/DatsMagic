d4e00747874c6b2b6e54516e0690b41bf6082c99
crimson-catfish <sava_kto@mail.ru>
2024-10-11 18:35:40 +0300

    test: bounties visualization

--- visualizer.py
+++ visualizer.py
@@ -3,14 +3,17 @@ import rewind_client, sys, os, json
 rc = rewind_client.RewindClient()
 
 # draw from file if file name was provided in args
+# to test this run rewind-viewer app, than:
+# python ./visualizer.py sample_log.json 
 if len(sys.argv) == 2:
     log_file_name = os.path.join("logs", sys.argv[1])
-    f = open("logs/" + log_file_name, "r", encoding="utf-8")
+    f = open(log_file_name, "r", encoding="utf-8")
     log = json.load(f)
 
     for frame in log["frames"]:
-        # read group type, draw it
-        pass
+        # draw_bounties(frame["bounties"])
+        
+        rc.end_frame()
 
 # something like blocks
 def draw_group_of_things():
