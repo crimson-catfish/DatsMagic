db10e5828e46af82264f472ceb672ab1c7cedc4a
crimson-catfish <sava_kto@mail.ru>
2024-10-11 19:10:34 +0300

    fix: visualizer test
    # Conflicts:
    #	visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -2,45 +2,31 @@ import rewind_client, sys, os, json
 import math as m
 import random
 
-a = 32 #for rectangle
+
+def draw_anomalies(anomalies_on_map):
+    for i in anomalies_on_map:
+        rc.circle(i["x"], i["y"], i["radius"], rc.BLUE)
+        rc.circle(i["x"], i["y"], i["effectiveRadius"], rc.DARK_BLUE)
+    pass
+
+
+def draw_bounties(bounties_on_map):
+    for i in bounties_on_map:
+        rc.circle(bounties_on_map[i]["x"], bounties_on_map[i]["y"], bounties_on_map[i]["radius"], rc.DARK_GREEN)
+
 
 rc = rewind_client.RewindClient()
-input()
 
 # draw from file if file name was provided in args
 # to test this run rewind-viewer app, than:
 # python ./visualizer.py sample_log.json 
 if len(sys.argv) == 2:
     log_file_name = os.path.join("logs", sys.argv[1])
+    print(log_file_name)
     f = open(log_file_name, "r", encoding="utf-8")
     log = json.load(f)
 
     for frame in log["frames"]:
-        # draw_bounties(frame["bounties"])
+        draw_bounties(frame["bounties"])
 
         rc.end_frame()
-
-
-# something like blocks
-def draw_anomalies(anomalies_on_map):
-    for i in anomalies_on_map:
-        rc.circle(i["x"]*a, i["y"]*a, i["radius"]*a, rc.BLUE)
-        rc.circle(i["x"]*a, i["y"]*a, i["effectiveRadius"]*a, rc.DARK_BLUE)
-    pass
-
-
-# something like enemies
-def draw_enemies(enemies_on_map):
-    for i in enemies_on_map:
-        rc.rectangle(i['x']*a, i['y']*a, i['x']*a + a, i['y']*a + a, rc.RED)
-    # rc.
-    pass
-
-
-def draw_bounties(bounties_on_map):
-    for i in bounties_on_map:
-        rc.circle(i["x"]*a, i["y"]*a, i["radius"]*a, rc.DARK_GREEN)
-
-
-
-
