bf06bf9b6d4c76dd2e694e244d419dfbba5bbdf6
crimson-catfish <sava_kto@mail.ru>
2024-10-11 21:33:53 +0300

    feat: log current state

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import api, frame_logger, visualizer
+import api, frame_logger, visualizer, rounds_utility
 
+print("collecting round info")
 rounds_info = api.rounds_info()
 current_round = rounds_info["now"]
+
+print("connecting to logger")
 logger = frame_logger.Logger(current_round)
+
+print("connecting to visualizer")
 vs = visualizer.Visualizer()
 
+print("connecting to game")
 frame = api.participate(test=True)
 
 while True:
     if "error" in frame:
         print("ERROR: ", frame["error"])
         if frame["error"] == 'realm not found':
-            print("сервер не работает(наверное)")
+            print("time before next round: " + str(rounds_utility.time_before_next_round(rounds_info)))
         break
+
     logger.log(frame)
     vs.draw_frame(frame)
     frame = api.participate()
