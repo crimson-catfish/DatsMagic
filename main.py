f7f23b3c3fb793b1b903c15789ab708d6b8d6cb6
crimson-catfish <sava_kto@mail.ru>
2024-10-11 19:54:02 +0300

    rename logger to frame_logger

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import api, logger
+import api, frame_logger
 from api import participate
 
 current_round = api.rounds_info()["now"]
-logger = logger.Logger(current_round)
+logger = frame_logger.Logger(current_round)
 
 frame = participate(test=True)
+logger.log(frame)
+logger.log(frame)
 
     
