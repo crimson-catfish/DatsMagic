a7d465e9fc4c69ca9fe4d2a466f3e9a1620ab906
crimson-catfish <sava_kto@mail.ru>
2024-10-11 18:46:24 +0300

    fix: visualizer test

--- visualizer.py
+++ visualizer.py
@@ -1,9 +1,14 @@
 import rewind_client, sys, os, json
 import math as m
 import random
+                
+
+def draw_bounties(bounties_on_map):
+    for i in bounties_on_map:
+        rc.circle(bounties_on_map[i]["x"], bounties_on_map[i]["y"], bounties_on_map[i]["radius"], rc.DARK_GREEN)
+
 
 rc = rewind_client.RewindClient()
-input()
 
 # draw from file if file name was provided in args
 # to test this run rewind-viewer app, than:
@@ -14,25 +19,6 @@ if len(sys.argv) == 2:
     log = json.load(f)
 
     for frame in log["frames"]:
-        # draw_bounties(frame["bounties"])
-        
+        draw_bounties(frame["bounties"])
+
         rc.end_frame()
-
-# something like blocks
-def draw_group_of_things():
-    # rc.
-    pass
-
-# something like enemies
-def draw_group_of_other_things():
-    # rc.
-    pass
-
-
-def draw_bounties(bounties_on_map):
-    for i in bounties_on_map:
-        rc.circle(bounties_on_map[i]["x"], bounties_on_map[i]["y"], bounties_on_map[i]["radius"], rc.DARK_GREEN)
-
-
-
-
