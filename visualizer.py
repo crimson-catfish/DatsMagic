44363342412a76e0432c24db2db39a8ba0683fff
tush00nka <endline00@ya.ru>
2024-10-11 22:25:20 +0300

    corrected transport radius, attack radius

--- visualizer.py
+++ visualizer.py
@@ -13,24 +13,14 @@ def clamp(val, a, b):
 class Visualizer:
     rc = rewind_client.RewindClient()
 
-    def draw_enemies(self, enemies_on_map):
+    def draw_enemies(self, enemies_on_map, transport_radius):
         for enemy in enemies_on_map:
             # calculate enemy direction
-            x_dir = enemy["velocity"]['x'] * CARPET_SIZE * 2
-            y_dir = enemy["velocity"]['y'] * CARPET_SIZE * 2
+            x_dir = enemy["velocity"]['x']
+            y_dir = enemy["velocity"]['y']
 
-            half_size = CARPET_SIZE/2
+            self.rc.circle(enemy['x'], enemy['y'], transport_radius, self.rc.RED, fill=True)
 
-            self.rc.circle(enemy['x'], enemy['y'], half_size, self.rc.RED, fill=True)
-
-            # left_point = [enemy['x'] - half_size, enemy['y'] + half_size]
-            # right_point = [enemy['x'] + half_size, enemy['y'] + half_size]
-
-            # draw enemy 's triangle rotated towards direction
-            # self.rc.triangle(left_point,
-            #                  right_point,
-            #                  [enemy['x'] + x_dir, enemy['y'] + y_dir],
-            #                  self.rc.RED, fill=True)
             # draw velocity vector
             self.rc.line(enemy["x"], enemy["y"], enemy['x'] + x_dir, enemy['y'] + y_dir, self.rc.DARK_RED)
 
@@ -45,7 +35,7 @@ class Visualizer:
             self.rc.circle(anomaly["x"], anomaly["y"], anomaly["effectiveRadius"], color, True)
             # draw velocity vector
             self.rc.line(anomaly["x"], anomaly["y"],
-                         anomaly["y"] + anomaly["velocity"]['x'],
+                         anomaly["x"] + anomaly["velocity"]['x'],
                          anomaly["y"] + anomaly["velocity"]['y'],
                          self.rc.DARK_BLUE)
 
@@ -53,34 +43,27 @@ class Visualizer:
         for bounty in bounties:
             self.rc.circle(bounty["x"], bounty["y"], bounty["radius"], self.rc.DARK_GREEN)
 
-    def draw_transports(self, transports_on_map):
+    def draw_transports(self, transports_on_map, transport_radius, attack_radius):
         for transport in transports_on_map:
-            # self.rc.rectangle(transport["velocity"]['x'], transport["velocity"]['y'],
-            #                   transport["velocity"]['x'] + 1, transport["velocity"]['y'] + 1, self.rc.GREEN)
 
             # calculate transport direction
-            x_dir = transport["velocity"]['x'] * CARPET_SIZE * 2
-            y_dir = transport["velocity"]['y'] * CARPET_SIZE * 2
+            x_dir = transport["velocity"]['x']
+            y_dir = transport["velocity"]['y']
 
-            half_size = CARPET_SIZE/2
-
-            self.rc.circle(transport['x'], transport['y'], half_size, self.rc.GREEN, fill=True)
-
-            # draw transport's triangle rotated towards direction
-            # self.rc.triangle([transport['x'] - half_size, transport['y'] - half_size],
-            #                  [transport['x'] + half_size, transport['y'] + half_size],
-            #                  [transport['x'] + x_dir, transport['y'] + y_dir],
-            #                  self.rc.GREEN, fill=True)
+            self.rc.circle(transport['x'], transport['y'], transport_radius, self.rc.GREEN, fill=True)
 
             # draw velocity vector
             self.rc.line(transport["x"], transport["y"], transport["x"] + x_dir, transport['y'] + y_dir,
                          self.rc.GREEN)
+
+            # draw shoot radius
+            self.rc.circle(transport['x'], transport['y'], attack_radius, self.rc.GREEN)
     
     def draw_frame(self, frame_to_draw: dict):
         self.draw_bounties(frame_to_draw["bounties"])
         self.draw_anomalies(frame_to_draw["anomalies"])
-        self.draw_enemies(frame_to_draw["enemies"])
-        self.draw_transports(frame_to_draw["transports"])
+        self.draw_enemies(frame_to_draw["enemies"], frame_to_draw["transportRadius"])
+        self.draw_transports(frame_to_draw["transports"], frame_to_draw["transportRadius"], frame_to_draw["attackRange"])
 
         self.rc.end_frame()
 
