3784dae2b0188d3adfc7756760dc4ea09cfd1890
crimson-catfish <sava_kto@mail.ru>
2024-10-11 19:46:50 +0300

    feat: logger

---
+++ logger.py
@@ -0,0 +1,16 @@
+import json
+
+
+class Logger:
+    def __init__(self, current_round: str):
+        self.file = open("logs/round" + current_round + ".json", 'w+')
+        self.file.write("{\"frames\": [\n")
+
+    def log(self, frame_data: dict):
+        frame_data_json = json.dumps(frame_data)
+        print(frame_data_json)
+        self.file.write(frame_data_json)
+
+    def __del__(self):
+        self.file.write("\n]}")
+        self.file.close()
