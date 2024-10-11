b3ffb687ed2b2e6116318a3cab401c65bc128adc
crimson-catfish <sava_kto@mail.ru>
2024-10-11 21:53:12 +0300

    fix: participation in one server

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import api, frame_logger, visualizer, rounds_utility
 
+test_or_not = True
+
 print("collecting round info")
 rounds_info = api.rounds_info()
 current_round = rounds_info["now"]
@@ -11,18 +13,18 @@ print("connecting to visualizer")
 vs = visualizer.Visualizer()
 
 print("connecting to game")
-frame = api.participate(test=True)
+frame = api.participate(test_or_not)
 
 while True:
     if "error" in frame:
-        print("ERROR: ", frame["error"])
+        print(frame["error"] + " " + str(frame["errCode"]))
         if frame["error"] == 'realm not found':
             print("time before next round: " + str(rounds_utility.time_before_next_round(rounds_info)))
         break
 
     logger.log(frame)
     vs.draw_frame(frame)
-    frame = api.participate()
+    frame = api.participate(test_or_not)
 
     # process frame
     
