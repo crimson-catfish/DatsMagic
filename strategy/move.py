2b9c953a72b9e20dfd67e790626b694839319593
crimson-catfish <sava_kto@mail.ru>
2024-10-12 11:44:01 +0300

    feat: don't hit walls

--- strategy/move.py
+++ strategy/move.py
@@ -1,3 +1,15 @@
 # TODO: process movement and don't kill yourself in the process
-def acceleration(frame: dict, transport: dict,  enemies_nearby: list):
-    return None
+def acceleration(frame: dict, transport: dict, enemies_nearby: list):
+    accel = {"x": 0, "y": 0}
+
+    if transport["x"] + transport["velocity"]["x"] > frame["mapSize"]["x"] * 0.99:
+        accel["x"] = -frame["maxAccel"]
+    elif transport["x"] + transport["velocity"]["x"] < frame["mapSize"]["x"] * 0.01:
+        accel["x"] = frame["maxAccel"]
+        
+    if transport["y"] + transport["velocity"]["y"] > frame["mapSize"]["y"] * 0.99:
+        accel["y"] = -frame["maxAccel"]
+    elif transport["y"] + transport["velocity"]["y"] < frame["mapSize"]["y"] * 0.01:
+        accel["y"] = frame["maxAccel"]
+
+    return accel
