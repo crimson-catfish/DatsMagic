a51a7482928f02f5debb7453ca08ef165a3b141b
crimson-catfish <sava_kto@mail.ru>
2024-10-12 12:22:16 +0300

    fix: comma in logger

--- frame_logger.py
+++ frame_logger.py
@@ -5,11 +5,16 @@ class Logger:
     def __init__(self, current_round: str):
         self.file = open("logs/round" + current_round.replace(":", ".") + ".json", 'w+')
         self.file.write("{\"frames\": [\n")
+        self.first = True
 
     def log(self, frame_data: dict):
         frame_data_json = json.dumps(frame_data)
-        self.file.write(frame_data_json + ",")
+        if not self.first:
+            self.file.write(",")
+        self.file.write(frame_data_json)
+        self.first = False
 
     def __del__(self):
+        self.file       
         self.file.write("\n]}")
         self.file.close()
