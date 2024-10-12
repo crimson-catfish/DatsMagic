e99efb7b8c1df13c00a272862169a00477ace966
crimson-catfish <sava_kto@mail.ru>
2024-10-12 12:50:33 +0300

    feat: scale to max accel

--- strategy/move.py
+++ strategy/move.py
@@ -1,5 +1,8 @@
 # TODO: process movement and don't kill yourself in the process
+import math
+
 from strategy import bounty_finder
+
 
 def clamp(val, a, b):
     if val < a:
@@ -10,16 +13,20 @@ def clamp(val, a, b):
     return val
 
 
+def scale_vector(vec: dict, size):
+    magnitude = math.sqrt(vec["x"] ** 2 + vec["y"] ** 2)
+    normalized_direction = {"x": vec["x"] / magnitude, "y": vec["y"] / magnitude}
+
+    return {"x": int(normalized_direction["x"] * size), "y": int(normalized_direction["y"] * size)}
+
+
 def acceleration(frame: dict, transport: dict, enemies_nearby: list):
     accel = {"x": 0, "y": 0}
 
     bounty_for_transport = bounty_finder.get_closest_bounty(frame["bounties"], frame["transports"])
     # TODO: write into accel to move to bounty
-    dir_x = bounty_for_transport[transport["id"]]['x'] - transport['x']
-    dir_y = bounty_for_transport[transport["id"]]['y'] - transport['y']
-
-    accel['x'] = clamp(dir_x,  -5,  5)
-    accel['y'] = clamp(dir_y,  -5,  5)
+    accel['x'] = bounty_for_transport[transport["id"]]['x'] - transport['x']
+    accel['y'] = bounty_for_transport[transport["id"]]['y'] - transport['y']
 
     # overwrite accel if we close to wall  
     if transport["x"] + transport["velocity"]["x"] > frame["mapSize"]["x"] * 0.95:
@@ -31,4 +38,4 @@ def acceleration(frame: dict, transport: dict, enemies_nearby: list):
     elif transport["y"] + transport["velocity"]["y"] < frame["mapSize"]["y"] * 0.05:
         accel["y"] = frame["maxAccel"]
 
-    return accel
+    return scale_vector(accel, frame["maxAccel"])
