2ede470ecc4d861d54b6f3fed24d0048f144ac33
crimson-catfish <sava_kto@mail.ru>
2024-10-12 13:23:05 +0300

    feat: move closer to center if no bounty close enough

--- strategy/move.py
+++ strategy/move.py
@@ -1,4 +1,3 @@
-# TODO: process movement and don't kill yourself in the process
 import math
 
 from strategy import bounty_finder
@@ -25,12 +24,14 @@ def clamp_vector(vec: dict, size):
 
 
 def acceleration(frame: dict, transport: dict, enemies_nearby: list):
-    accel = {"x": 0, "y": 0}
+    # move to center by default
+    accel = {"x": frame["mapSize"]["x"] / 2 - transport["x"],
+             "y": frame["mapSize"]["y"] / 2 - transport["y"]}
 
     bounty_for_transport = bounty_finder.get_closest_bounty(frame["bounties"], frame["transports"])
-    # TODO: write into accel to move to bounty
-    accel['x'] = bounty_for_transport[transport["id"]]['x'] - transport['x']
-    accel['y'] = bounty_for_transport[transport["id"]]['y'] - transport['y']
+    if bounty_for_transport[transport["id"]]["x"] is not None:
+        accel['x'] = bounty_for_transport[transport["id"]]['x'] - transport['x']
+        accel['y'] = bounty_for_transport[transport["id"]]['y'] - transport['y']
 
     # overwrite accel if we close to wall  
     if transport["x"] + transport["velocity"]["x"] > frame["mapSize"]["x"] * 0.95:
