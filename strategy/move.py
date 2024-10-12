9cc8835b420f942c80e7d8fd80d2143b751569a4
crimson-catfish <sava_kto@mail.ru>
2024-10-12 11:59:37 +0300

    minor

--- strategy/move.py
+++ strategy/move.py
@@ -3,17 +3,16 @@ from strategy import bounty_finder
 
 
 def acceleration(frame: dict, transport: dict, enemies_nearby: list):
-
-    bounty_for_transport = bounty_finder.get_closest_bounty(frame["bounties"], frame["transports"])
-    print(bounty_for_transport)
-    
     accel = {"x": 0, "y": 0}
 
+    bounty_for_transport = bounty_finder.get_closest_bounty(frame["bounties"], frame["transports"])
+    # TODO: write into accel to move to bounty
+
+    # overwrite accel if we close to wall  
     if transport["x"] + transport["velocity"]["x"] > frame["mapSize"]["x"] * 0.99:
         accel["x"] = -frame["maxAccel"]
     elif transport["x"] + transport["velocity"]["x"] < frame["mapSize"]["x"] * 0.01:
         accel["x"] = frame["maxAccel"]
-        
     if transport["y"] + transport["velocity"]["y"] > frame["mapSize"]["y"] * 0.99:
         accel["y"] = -frame["maxAccel"]
     elif transport["y"] + transport["velocity"]["y"] < frame["mapSize"]["y"] * 0.01:
